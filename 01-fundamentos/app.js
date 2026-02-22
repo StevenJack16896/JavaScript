519e781e122694a319b6e6de4f8cfc9563092817
StevenJack16896 <eamon_16896@hotmail.com>
2026-02-22 18:50:49 -0500

    nuevos comandos

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -1,3 +1,22 @@
 let a = 10;
 var b = 20;
 const c = 15;
+
+let z = 30,zy= 40, zx = 50;
+let p = 'Hola';
+let p1 = 'Soy Spiderman';
+console.log(a)
+console.log(b)
+console.log(c)
+
+
+console.log('%c Mis variables', 'color: red; font-weight: bold; font-size: 20px;');
+
+
+console.log({a})
+console.log({b});
+console.log({c});
+
+console.error(a);
+console.warn(b);
+console.table({a, b, c});
